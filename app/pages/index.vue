1fff18cf7d84236b0917ceac2ea63f75e50695fa
hadiasta <hijrihadi@gmail.com>
2025-09-04 07:23:07 +0700

    feat : test on mount cycle on nuxt 3 / 4

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component aaaxxxxxxxxxxxxxxx </AppAlert>
+    <AppAlert>
+      This is an auto-imported component aaaxxxxxxxxxxxxxxx
+    </AppAlert>
     <div>
       <Child :nama="namaUser" />
     </div>
   </div>
 </template>
 
-
 <script setup>
+import { onMounted } from 'vue' // Vue Composition API
 
 const namaUser = 'hadi'
+
+const consoleMe = () => {
+  console.log("wow")
+}
+
+onMounted(() => {
+  consoleMe()
+})
 </script>
